ef749932d9382a9d7cba1a853b76740266f63378
Will Johnston <will.johnston@kroger.com>
2023-11-27 15:32:04 -0800

    Add Timer component with styles

--- Components/Timer/Timer.tsx
+++ Components/Timer/Timer.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import {Button, SafeAreaView, FlatList, View, Text} from 'react-native';
 
 import {store} from '../../System/Store';
 import {useTimer} from '../../hooks/useTimer';
+import {styles} from './Timer.style';
 
 export const Timer = (): React.ReactElement => {
   const clearStore = (): void => {
@@ -12,14 +13,32 @@ export const Timer = (): React.ReactElement => {
 
   const tTime = useTimer(store.getMeal());
 
-  console.log(tTime);
-
   return (
     <SafeAreaView>
-      <View>
-        <Text>Timer</Text>
-        <Button title="Clear Store" onPress={clearStore} />
+      <View style={styles.TimerWrapper}>
+        <FlatList
+          data={Object.entries(tTime).filter(
+            item => item[1] !== 0 && !['seconds'].includes(item[0]),
+          )}
+          renderItem={({index, item}) => <TimerItem item={item} />}
+          horizontal={true}
+        />
       </View>
+      <Button title="Clear Store" onPress={clearStore} />
     </SafeAreaView>
+  );
+};
+
+type TimerItemProps = {
+  item: [string, number];
+};
+
+const TimerItem = ({item}: TimerItemProps) => {
+  const [unit, value] = item;
+  return (
+    <View style={styles.TimerItemWrapper}>
+      <Text style={styles.TimerDigit}>{value}</Text>
+      <Text style={styles.TimerUnit}>{unit.toUpperCase()}</Text>
+    </View>
   );
 };
